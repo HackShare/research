821dd6103324f935e7a4e85aeb224baf549af83f
middle59 <callmemike94@aol.com>
2015-09-14 16:46:28 -0400

    Update runDriver.sh

--- TestDrivers/HADS/bin/runDriver.sh
+++ TestDrivers/HADS/bin/runDriver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd $HOME/test/HADS/
+cd $HOME/TestDrivers/HADS/
 echo "pwd=`pwd`"
-CLASSPATH=.:$HOME/test:$HOME/hazelcast-3.5.2/lib/hazelcast-3.5.2.jar java Driver
+CLASSPATH=.:$HOME/TestDrivers:$HOME/hazelcast-3.5.2/lib/hazelcast-3.5.2.jar java Driver
