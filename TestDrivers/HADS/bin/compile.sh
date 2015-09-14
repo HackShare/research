d1ee65653411620e3339e553fa5c624207c6b6f2
middle59 <callmemike94@aol.com>
2015-09-14 16:44:22 -0400

    Create compile.sh

---
+++ TestDrivers/HADS/bin/compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export WHERE=$HOME
+export CLASSPATH=.:$WHERE/test:$WHERE/hazelcast-3.5.2/lib/hazelcast-3.5.2.jar
+
+cd $WHERE/test
+
+cd HADS/Server
+javac *.java
+cd ../..
+
+cd HADS
+javac Driver.java
+cd ../..
