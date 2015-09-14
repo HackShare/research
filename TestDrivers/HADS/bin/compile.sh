3bbdeae2aed781b441d3bb157cccbe307a5baaeb
middle59 <callmemike94@aol.com>
2015-09-14 16:46:00 -0400

    Update compile.sh

--- TestDrivers/HADS/bin/compile.sh
+++ TestDrivers/HADS/bin/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WHERE=$HOME
-export CLASSPATH=.:$WHERE/test:$WHERE/hazelcast-3.5.2/lib/hazelcast-3.5.2.jar
+export CLASSPATH=.:$WHERE/TestDrivers:$WHERE/hazelcast-3.5.2/lib/hazelcast-3.5.2.jar
 
 cd $WHERE/test
 
